a71275ff317458f730bff148ba5c6a87743537a4
sin392 <sin392@gmail.com>
2022-10-02 16:13:26 +0000

    update candidate filtering

--- scripts/modules/grasp.py
+++ scripts/modules/grasp.py
@@ -2,15 +2,17 @@ from typing import List, Tuple
 
 import cv2
 import numpy as np
+import rospy
 
 
 class ParallelCandidate:
-    def __init__(self, p1, p2, pc, depth, h, w, contour):
-        # TOFIX: ptにそのままp1, p2をわたすと何故かエラー
+    def __init__(self, p1, p2, pc, depth, h, w, contour, finger_radius=1):
+        # TODO: フィルタリングに関しては1点のdepthではなく半径finger_radius内の領域のdepthの平均をとるべきかも
         self.h = h
         self.w = w
         self.contour = contour
         self.depth = depth
+        self.finger_radius = finger_radius
         self.p1_u, self.p1_v, self.p1_d = self._format(p1)
         self.p2_u, self.p2_v, self.p2_d = self._format(p2)
         self.pc_u, self.pc_v, self.pc_d = self._format(pc)
@@ -25,8 +27,9 @@ class ParallelCandidate:
     def validate(self):
         is_invalid = False
         is_invalid = is_invalid or self._is_outside_frame()
-        # is_invalid = is_invalid or self._is_in_mask()
-        is_invalid = is_invalid or self._is_center_above_points()
+        is_invalid = is_invalid or self._is_depth_missing()
+        is_invalid = is_invalid or self._is_in_mask()
+        is_invalid = is_invalid or self._is_center_under_edges()
         return not is_invalid
 
     def get_candidate_points_on_rgbd(self):
@@ -45,17 +48,33 @@ class ParallelCandidate:
             return True
         return False
 
-    def _is_in_mask(self):
-        """把持点がマスクにかぶっていればスキップ"""
-        if cv2.pointPolygonTest(self.contour, (self.p1_u, self.p1_v), measureDist=False):
-            return True
-        if cv2.pointPolygonTest(self.contour, (self.p2_u, self.p2_v), measureDist=False):
+    def _is_depth_missing(self) -> bool:
+        """depthが取得できていない(値が0以下)の場合はスキップ"""
+        if self.p1_d <= 0 or self.p2_d <= 0 or self.pc_d <= 0:
             return True
         return False
 
-    def _is_center_above_points(self):
-        """中心のdepthが把持点のdepthよりも低ければスキップ"""
-        return self.pc_d <= min(self.p1_d, self.p2_d)
+    def _is_in_mask(self, margin=1):
+        """把持点がマスク内部に位置する場合はスキップ"""
+        # measureDict=Trueのときpolygonの内側にptが存在する場合正の距離、輪郭上で０、外側で負の距離
+        # TODO: marginをfinger_radiusから決定
+        pt1_inner_dist = cv2.pointPolygonTest(self.contour, (self.p1_u, self.p1_v), measureDist=True)
+        if pt1_inner_dist - margin > 0:
+            return True
+        pt2_inner_dist = cv2.pointPolygonTest(self.contour, (self.p2_u, self.p2_v), measureDist=True)
+        if pt2_inner_dist - margin > 0:
+            return True
+        return False
+
+    def _is_center_under_edges(self):
+        """
+        中心のdepthが把持点の深い方のdepthより深ければ（値が大きければ）スキップ
+        * カメラと作業領域に傾きがある場合を考慮して２点のより深い(値が大きい)方を比較対象に
+        * marginはdepth自体の誤差を考慮
+        """
+        # WARN: カメラと作業領域に傾きがある場合
+        deeper_pt = max(self.p1_d, self.p2_d)
+        return self.pc_d > deeper_pt
 
 
 class ParallelGraspDetector:
