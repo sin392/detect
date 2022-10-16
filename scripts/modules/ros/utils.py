fix wrong index

scripts/modules/ros/utils.py
@@ -34,8 +34,8 @@ class PointProjector:
 
     def get_length_between_2d_points(self, pt1_2d: Tuple[int, int], pt2_2d: Tuple[int, int], depth: np.ndarray):
         """スクリーン座標系の二点をカメラ座標系に投影し、二点間の長さ[mm]を算出"""
-        pt1_3d_c = self.screen_to_camera(pt1_2d, depth[pt1_2d[0], pt1_2d[1]])
-        pt2_3d_c = self.screen_to_camera(pt2_2d, depth[pt2_2d[0], pt1_2d[1]])
+        pt1_3d_c = self.screen_to_camera(pt1_2d, depth[pt1_2d[1], pt1_2d[0]])
+        pt2_3d_c = self.screen_to_camera(pt2_2d, depth[pt2_2d[1], pt1_2d[0]])
         distance = self.get_length_between_3d_points(pt1_3d_c, pt2_3d_c)
 
         return distance