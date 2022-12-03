4fb4239fe0c228d8f99c63177bda867b85c924f0
sin392 <sin392@gmail.com>
2022-12-03 13:36:45 +0000

    tmp: candidatesの評価追加

--- scripts/experiments/check_saved_pickle.py
+++ scripts/experiments/check_saved_pickle.py
@@ -128,37 +128,79 @@ print("mean value", np.mean(depth_values_in_mask))
 # %%
 
 
-def compute_mean_depth_in_finger_area(depth, pt_xy, radius):
+def compute_depth_profile_in_finger_area(depth, pt_xy, radius):
     x_slice = slice(pt_xy[0] - radius, pt_xy[0] + radius + 1)
     y_slice = slice(pt_xy[1] - radius, pt_xy[1] + radius + 1)
     cropped_depth = depth[x_slice, y_slice]
     finger_mask = np.zeros_like(cropped_depth, dtype=np.uint8)
     cv2.circle(finger_mask, (cropped_depth.shape[0] // 2, cropped_depth.shape[1] // 2), radius, 255, -1)
     depth_values_in_mask = cropped_depth[finger_mask == 255]
-    return int(np.mean(depth_values_in_mask))
+    return int(np.min(depth_values_in_mask)), int(np.max(depth_values_in_mask)), int(np.mean(depth_values_in_mask))
 
 
-print(compute_mean_depth_in_finger_area(depth, edge_xy[::-1], 10))
+print(compute_depth_profile_in_finger_area(depth, edge_xy[::-1], 10))
 # %%
 radius = 10
 cnt = 0
 mean_depth_list = []
+scores = []
+
+# 対象周辺の仮の最大/最小深度
+assumed_min_depth = depth[merged_mask == 255].min()
+assumed_max_depth = depth.max()
+print(assumed_min_depth, assumed_max_depth)
+
 for points in target_candidates:
     for u, v in points:
-        mean_depth = compute_mean_depth_in_finger_area(depth, (v, u), radius)
+        min_depth, max_depth, mean_depth = compute_depth_profile_in_finger_area(depth, (v, u), radius)
         mean_depth_list.append(mean_depth)
+        # 正規化しないと最大値、最小値の間の差が小さい -> ２乗
+        # ただcandidate個別の最小値、最大値をつかってmin-max正規化してしまうとcandidate間での基準が変わってしまうのでは？
+        # →　一旦ここではcandidate内での比較にとどめておきつつ、オリジナルな値を保存しておいてあとでひかくとかでもいいかも
+        # 先に全体マスク領域+アルファ(explode)の領域から仮の最大・最小をもとめてもいいかも
+        # TOFIX: 最大・最小の差がないときに最も大きな値となってしまう
+        score = ((mean_depth - assumed_min_depth) / (assumed_max_depth - assumed_min_depth + 1e-6)) ** 2
+        scores.append(score)
         cnt += 1
 
 print(mean_depth_list)
+print(scores)
 
 # 指の範囲も含めたinsertion pointの深さ判定
 # 実際にはスコアとして計算
 deep_points_2 = [flatten_points[i] for i in range(len(flatten_points)) if mean_depth_list[i] >= 1076]
-print(len(deep_points_2))
 
 candidate_img_3 = candidate_img.copy()
 for pt in deep_points_2:
     cv2.circle(candidate_img_3, pt, 3, (0, 255, 0), -1)
     cv2.circle(candidate_img_3, pt, 10, (0, 100, 100), 1, cv2.LINE_AA)
 plt.imshow(candidate_img_3)
+# %%
+# 指の範囲も含めたinsertion pointの深さ判定
+print(scores)
+deep_points_3 = [flatten_points[i] for i in range(len(flatten_points)) if scores[i] >= 0.95]
+
+candidate_img_4 = candidate_img.copy()
+for pt in deep_points_3:
+    cv2.circle(candidate_img_4, pt, 3, (0, 255, 0), -1)
+    cv2.circle(candidate_img_4, pt, 10, (0, 100, 100), 1, cv2.LINE_AA)
+plt.imshow(candidate_img_4)
+# %%
+# 理想的にはinsertion pointとcontact pointの組み合わせからなるパーツで評価したほうがいいが、
+# 組み合わせ角度が一定の場合は先に組み合わせ時のスコア評価してフィルタリングしたほうが計算減ってよいかも
+# → そもinsertion pointのスコアが低い場合contact point計算しなくてよい
+print(target_candidates)
+finger_num = len(target_candidate)
+candidates_scores = [np.prod(scores[i:i + 4]) for i in range(0, len(scores), finger_num)]
+print(candidates_scores)
+
+best_candidate = target_candidates[np.argmax(candidates_scores)]
+print(best_candidate)
+
+candidate_img_5 = candidate_img.copy()
+for edge in best_candidate:
+    cv2.line(candidate_img_5, target_center, np.int0(edge), (255, 0, 0), thickness, cv2.LINE_AA)
+
+plt.imshow(candidate_img_5)
+
 # %%
